15a5f25e73d58b142ec2936344e82f0f329dfddd
zhaozhe <dgxdks@163.com>
2023-09-19 20:53:43 +0800

    1.修复数据权限拦截器无法解析对应SQL的问题

--- youlan-tools/src/main/java/com/youlan/tools/mapper/GeneratorTableMapper.java
+++ youlan-tools/src/main/java/com/youlan/tools/mapper/GeneratorTableMapper.java
@@ -1,5 +1,6 @@
 package com.youlan.tools.mapper;
 
+import com.baomidou.mybatisplus.annotation.InterceptorIgnore;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.youlan.tools.entity.DBTable;
@@ -7,9 +8,8 @@ import com.youlan.tools.entity.GeneratorTable;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-import java.util.List;
-
 @Mapper
 public interface GeneratorTableMapper extends BaseMapper<GeneratorTable> {
-    IPage<DBTable> getDbTableList(@Param("page") IPage<DBTable> page, @Param("dt") DBTable dbTable);
+    @InterceptorIgnore
+    IPage<DBTable> getDbTablePageList(@Param("page") IPage<DBTable> page, @Param("dt") DBTable dbTable);
 }
